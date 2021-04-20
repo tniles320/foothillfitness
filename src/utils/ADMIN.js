c97d4091b462e09512b767a58590161025982813
Taylor Niles <tniles320@gmail.com>
2021-04-20 16:33:24 -0700

    adding headers to admin axios calls

--- src/utils/ADMIN.js
+++ src/utils/ADMIN.js
@@ -3,15 +3,27 @@ import axios from "axios";
 
 const URL = "https://us-central1-foothill-fitness.cloudfunctions.net/app";
 
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "https://foothillfitness.com",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  "Access-Control-Allow-Headers": "X-Requested-With,content-type",
+  "Access-Control-Allow-Credentials": true,
+};
+
 export default {
   // Gets user info
   getUser: function () {
-    return axios.get(`${URL}/admin/user`, { withCredentials: true });
+    return axios.get(`${URL}/admin/user`, {
+      withCredentials: true,
+      headers: headers,
+    });
   },
   // Logs the user out
   logout: function (id) {
     return axios.post(`${URL}/admin/user/logout`, {
       withCredentials: true,
+      headers: headers,
     });
   },
   // Log the user in
@@ -19,7 +31,10 @@ export default {
     return axios.post(
       `${URL}/admin/user/login`,
       { username, password },
-      { withCredentials: true }
+      { withCredentials: true },
+      {
+        headers: headers,
+      }
     );
   },
   // New user registration
