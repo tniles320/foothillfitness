c6356bcee75ab5b72b52d76bb334387d9e51bf09
Taylor Niles <tniles320@gmail.com>
2021-04-20 17:39:37 -0700

    updating firebase.json

--- src/utils/ADMIN.js
+++ src/utils/ADMIN.js
@@ -3,28 +3,17 @@ import axios from "axios";
 
 const URL = "https://us-central1-foothill-fitness.cloudfunctions.net/app";
 
-const headers = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin":
-    "https://us-central1-foothill-fitness.cloudfunctions.net/app",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-  "Access-Control-Allow-Headers": "X-Requested-With,content-type",
-  "Access-Control-Allow-Credentials": true,
-};
-
 export default {
   // Gets user info
   getUser: function () {
     return axios.get(`${URL}/admin/user`, {
       withCredentials: true,
-      headers: headers,
     });
   },
   // Logs the user out
   logout: function (id) {
     return axios.post(`${URL}/admin/user/logout`, {
       withCredentials: true,
-      headers: headers,
     });
   },
   // Log the user in
@@ -32,10 +21,7 @@ export default {
     return axios.post(
       `${URL}/admin/user/login`,
       { username, password },
-      { withCredentials: true },
-      {
-        headers: headers,
-      }
+      { withCredentials: true }
     );
   },
   // New user registration
